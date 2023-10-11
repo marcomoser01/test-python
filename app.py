2b7fe3d6d3e40eec883ee15c7d42feb5acc83adb
marco.moser <marco.moser@mat.tn.it>
2023-10-11 16:19:57 +0200

    .

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    
+    st.header("Chat with multiple PDFs ::books:")
+
+if __name__ == '__main__':
+    main()
