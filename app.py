6791aacbe8eccfaaec9e30681cb46cfa62826b21
marco.moser <marco.moser@mat.tn.it>
2023-10-12 10:46:31 +0200

    .

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceBgeEmbeddings
 from langchain.vectorstores import FAISS
 
 def get_pdf_text(pdf_docs):
@@ -25,7 +25,8 @@ def get_text_chuncks(text):
     return chunks
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceBgeEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings )
     return vectorstore
 
