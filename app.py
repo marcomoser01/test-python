ca5d737b0311958ea580a79c0b2c06bcb7f6240c
marco.moser <marco.moser@mat.tn.it>
2023-10-12 09:39:21 +0200

    .

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ def main():
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
     
     st.header("Chat with multiple PDFs ::books:")
+    st.text_input("Ask a question about your documents:")
+    
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here and click on Process", accept_multiple_files=True)
+    
 
 if __name__ == '__main__':
     main()
